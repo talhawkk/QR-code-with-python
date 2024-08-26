c9d3cd7886800c6cc5a3b871c53b0019e7f3ebbe
Talhak <talhawkk@gmail.com>
2024-08-26 15:30:16 +0500

    savig qr, adding extension and final print statement

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ try:
     data = input("Please enter the data you want to encode in the QR code: \n")
     name = input("kindly enter name for QR code (without extentsion) :\n")
     img=qr.make(data)
-    
-
+    img.save(name+".png")
+    print(f"your QR code has been saved with name {name}.png")
 except Exception as e:
     print(f"An error is occured {e}")
 
