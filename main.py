f7fd49279a7a6ac8559bd3243c219d97019972a2
Talhak <talhawkk@gmail.com>
2024-08-26 15:55:19 +0500

    Adding feature of save dialog prompt and debuging

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import qrcode as qr
+import tkinter as tk
+from tkinter import filedialog
 try:
-    import qrcode as qr
+    root = tk.Tk()
+    root.withdraw()
     data = input("Please enter the data you want to encode in the QR code: \n")
     name = input("kindly enter name for QR code (without extentsion) :\n")
     img=qr.make(data)
-    img.save(name+".png")
-    print(f"your QR code has been saved with name {name}.png")
+    savepath=filedialog.asksaveasfilename(defaultextension=".png",initialfile=name,filetypes=[("PNG files","*.png")])
+    # img.save(name+".png")
+
+    if savepath:
+        img.save(savepath)
+        print(f"your QR code has been saved with name {name}.png")
+    else:
+        print("operation failed")
 except Exception as e:
     print(f"An error is occured {e}")
 
